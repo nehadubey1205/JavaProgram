5ce96332d4c3bac9cf269979b8e36c16667f763a
nehadubey1205 <nehadubey1205@gmail.com>
2022-07-03 16:33:42 +0530

    first commit

--- SwapNumber.java
+++ SwapNumber.java
@@ -5,11 +5,20 @@ public class SwapNumber{
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
         int b = scn.nextInt();
-        int temp = a;
+        /*a = a+b;
+        b = a-b;
+        a = a-b;*/
+        /*a = a^b;
+        b = a^b;
+        a = a^b;*/
+        a = a*b;
+        b = a/b;
+        a = a/b;
+        /*int temp = a;
 
         a = b;
         b = temp;
-       // int tem = scn.nextInt();
+       // int tem = scn.nextInt();*/
        System.out.println("after swap:" + a + " " + b);
        
 
