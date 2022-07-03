8f0801d5fbe793500feecdc8103ef15762b463c2
nehadubey1205 <93631179+nehadubey1205@users.noreply.github.com>
2022-07-03 22:45:37 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println(a+b);
+    }
+}
